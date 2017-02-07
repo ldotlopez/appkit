71f8b34a922f874efcdf3acd8cdba19d477225e9
Luis López <ldotlopez@gmail.com>
2017-02-07 13:02:19 +0100

    Build stuff

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from setuptools import setup
 
-fh = open("requirements.txt")
-pkgs = filter(lambda line: line and line[0] >= 'a' and line[0] <= 'z',
-              fh.readlines())
-pkgs = list(pkgs)
+with open("requirements.txt") as fh:
+    pkgs = fh.readlines()
+
+pkgs = [x.strip() for x in pkgs]
+pkgs = [x for x in pkgs if x and x[0] != '#']
 
 setup(
     name='appkit',
     version='0.9.0',
     author='Luis López',
     author_email='ldotlopez@gmail.com',
-    packages=['appkit'],
+    packages=['appkit', 'appkit.application', 'appkit.db'],
     scripts=[],
     url='https://github.com/ldotlopez/appkit',
     license='LICENSE.txt',
